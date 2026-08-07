0f7ea96cabbd554057773b3c16714504141bc4d0
ay149739-lang <ay149739@gmail.com>
2026-08-07 16:06:22 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const http = require("http");
+const http = require("http");  
 const cors = require("cors");
 const { Server } = require("socket.io");
 const setupSocket = require("./socket/socketHandler");
