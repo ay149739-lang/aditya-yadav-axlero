27fd249c4d78d1bce37974d4280c1d43672ceaf4
ay149739-lang <ay149739@gmail.com>
2026-08-12 22:35:14 +0530

    Fix formatting of draw-start socket event handler

--- server/socket/socketHandler.js
+++ server/socket/socketHandler.js
@@ -50,7 +50,7 @@ module.exports = (io) => {
     });
 
     // Week 2 Day 1: Placeholder socket events
-    socket.on("draw-start", (data) => {
+    socket.on("draw-start", (data) =>{
       const room = rooms.get(data.roomId);
       if (room) room.shapes.push(data);
       socket.to(data.roomId).emit("draw-start", data); 
